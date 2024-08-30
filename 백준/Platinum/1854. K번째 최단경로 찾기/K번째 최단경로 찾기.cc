5d44010d4f95033c27be023c4c1f6ccf15d5f4cd
GK <64512313+Guho-Kim@users.noreply.github.com>
2024-08-30 18:27:51 +0900

    [Platinum IV] Title: K번째 최단경로 찾기, Time: 80 ms, Memory: 6092 KB -BaekjoonHub

--- 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.cc
+++ 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.cc
@@ -1,71 +1,69 @@
-#include <iostream>
-#include <vector>
-#include <queue>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
 
 using namespace std;
+#define MAX_N 1002
 
-typedef struct Node {
-	int dest;
-	int cost;
+typedef struct Node{
+    int dest;
+    int cost;
 }Node;
 
-int N, M, K;
-vector< Node > adj[1002];
-priority_queue<int> kth_pq[1002];
 struct compare {
 	bool operator()(const Node &a, const Node &b) {
 		return a.cost > b.cost;
 	}
 };
-void dijkstra() {
-	priority_queue< Node, vector<Node>, compare > pq;
-	pq.push({ 1, 0 });
-	kth_pq[1].push(0);
 
-	while (!pq.empty()) {
-		auto cur = pq.top(); pq.pop();
-		int src = cur.dest;
+vector<Node> graph[MAX_N];
+priority_queue<int> kth_pq[MAX_N];
+int N, M, K;
 
-		for (auto next : adj[src]) {
-			int dest = next.dest;
-			int cost = next.cost + cur.cost;
-
-			if (kth_pq[dest].size() < K) {
-				kth_pq[dest].push(cost);
-				pq.push({ dest, cost });
-			}
-			else if (kth_pq[dest].top() > cost) {
-				kth_pq[dest].pop();
-				kth_pq[dest].push(cost);
-				pq.push({ dest, cost });
-			}
-		}
-	}
+void init(){
+    cin >> N >> M >> K;
+    for(int i = 0; i < M; i++){
+        int a, b, c;
+        cin >> a >> b >> c;
+        graph[a].push_back({b, c});
+    }
 }
 
+void dijkstra(int start){
+    priority_queue<Node, vector<Node>, compare> pq;
+    pq.push({start, 0});
+    kth_pq[1].push(0);
+    while(!pq.empty()){
+        Node cur = pq.top(); pq.pop();
+        for(auto next : graph[cur.dest]){
+            int dest = next.dest;
+            int cost = next.cost + cur.cost;
+            if(kth_pq[dest].size() < K){
+                kth_pq[dest].push(cost);
+                pq.push({dest, cost});
+            }else if(kth_pq[dest].top() > cost){
+                kth_pq[dest].pop();
+                kth_pq[dest].push(cost);
+                pq.push({dest, cost});
+            }
+        }
+    }
+}
 
-int main()
-{
+void solve(){
+    dijkstra(1);
+    for(int i = 1; i <= N; i++){
+        if(kth_pq[i].size() < K) cout << -1 << '\n';
+        else cout << kth_pq[i].top() << '\n';
+    }
 
-	ios_base::sync_with_stdio(0);
-	cin.tie(0), cout.tie(0);
+}
 
-	cin >> N >> M >> K;
-
-	for (int i = 0; i < M; i++) {
-		int u, v, d;
-
-		cin >> u >> v >> d;
-
-		adj[u].push_back({v,d});
-	}
-
-	dijkstra();
-
-	for (int i = 1; i <= N; i++) {
-		if (kth_pq[i].size() != K) cout << -1 << "\n";
-		else cout << kth_pq[i].top() << "\n";
-	}
-
-	return 0;
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    // freopen("input.txt", "r", stdin);
+    init();
+    solve();
+    return 0;
 }
