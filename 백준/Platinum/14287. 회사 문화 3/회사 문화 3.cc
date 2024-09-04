f55e96eb3c6e80f2814bf6c90d89d33bb3cd0a14
GK <64512313+Guho-Kim@users.noreply.github.com>
2024-09-04 17:00:00 +0900

    [Platinum III] Title: 회사 문화 3, Time: 56 ms, Memory: 13920 KB -BaekjoonHub

--- 백준/Platinum/14287. 회사 문화 3/회사 문화 3.cc
+++ 백준/Platinum/14287. 회사 문화 3/회사 문화 3.cc
@@ -1,73 +1,79 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef pair<ll, ll> pll;
 #define MAX_N 100001
-// const ll N=1000000;
-ll n,m;
-vector<ll> graph[MAX_N];
-pll Qu[MAX_N]; 
-ll cnf=0,p;
-vector<ll>segTree(MAX_N*4);
-vector<ll>tree1(MAX_N*4);
+int n,m;
+int cnt=0;
+int s[MAX_N], e[MAX_N];
+vector<int> graph[MAX_N];
+vector<int>segTree(MAX_N*4);
+vector<int>lazy(MAX_N*4);
 
-void dfs(ll v){
-    Qu[v].first = ++cnf;
-    for(ll nv : graph[v]){
+void dfs(int v){
+    s[v] = ++cnt;
+    for(int nv : graph[v]){
         dfs(nv);
     }
-    Qu[v].second=cnf;
+    e[v]=cnt;
 }
-ll update_segment(ll n,ll s,ll e){
-    if(s==e){return segTree[n]=0;}
-    return segTree[n]=update_segment(n*2,s,(s+e)/2)+update_segment(n*2+1,(s+e)/2+1,e);
+
+int create_segment(int node,int s,int e){
+    if(s==e){return segTree[node]=0;}
+    return segTree[node]=create_segment(node*2,s,(s+e)/2)+create_segment(node*2+1,(s+e)/2+1,e);
 }
-void update_lazy(ll n,ll s,ll e){
-    if(tree1[n]!=0){
+void update_lazy(int node,int s,int e){
+    if(lazy[node]!=0){
+        segTree[node]+=lazy[node]*(e-s+1);
         if(s!=e){
-            tree1[n*2]+=tree1[n];
-            tree1[n*2+1]+=tree1[n];
+            lazy[node*2]+=lazy[node];
+            lazy[node*2+1]+=lazy[node];
         }
-        segTree[n]+=tree1[n]*(e-s+1);
-        tree1[n]=0;
+        lazy[node]=0;
     }
 }
-ll max1(ll n,ll s,ll e,ll left,ll right){
+int query(int n,int s,int e,int left,int right){
     update_lazy(n,s,e);
-    if(left>e||right<s){return 0;}
-    if(left<=s&&e<=right){return segTree[n];}
-    return max1(n*2,s,(s+e)/2,left,right)+max1(n*2+1,(s+e)/2+1,e,left,right);
+
+    if(left>e||right<s) return 0;
+    
+    if(left<=s&&e<=right) return segTree[n];
+
+    return query(n*2,s,(s+e)/2,left,right)+query(n*2+1,(s+e)/2+1,e,left,right);
 }
-void change(ll n,ll s,ll e,ll s1,ll e1,ll f){
+void update_range(int n,int s,int e,int sidx,int eidx,int diff){
     update_lazy(n,s,e);
-    if(s1<=s&&e<=e1){tree1[n]+=f;update_lazy(n,s,e);return;}
-    if(e1<s||s1>e){return;}
-    change(n*2,s,(s+e)/2,s1,e1,f);
-    change(n*2+1,(s+e)/2+1,e,s1,e1,f);
+    
+    if(eidx<s||sidx>e) return;
+
+    if(sidx<=s&&e<=eidx){
+        lazy[n]+=diff;
+        update_lazy(n,s,e);
+        return;
+    }
+
+    update_range(n*2,s,(s+e)/2,sidx,eidx,diff);
+    update_range(n*2+1,(s+e)/2+1,e,sidx,eidx,diff);
     segTree[n]=segTree[n*2]+segTree[n*2+1];
 }
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0),cout.tie(0);
     cin >> n >> m;
-    ll q,c,d;
+    int q,c,d,p;
     for(int i = 1; i <= n; i++){
         cin >> p;
-        if(p!=-1){graph[p].push_back(i);}
+        if(p!=-1)
+            graph[p].push_back(i);
     }
     dfs(1);
-    //for(int i=1;i<=n;i++){
-    //    cout << Qu[i].first << ' ' << Qu[i].second <<'\n';
-    //}
-    update_segment(1,1,n);
+    create_segment(1,1,n);
     for(int i=0;i<m;i++){
         cin >> q;
         if(q==1){
             cin >> c >> d;
-            change(1,1,n,Qu[c].first,Qu[c].first,d);
+            update_range(1,1,n,s[c],s[c],d);
         }else{
             cin >> c;
-            cout << max1(1,1,n,Qu[c].first,Qu[c].second) << '\n';
+            cout << query(1,1,n,s[c],e[c]) << '\n';
         }
     }
 }
