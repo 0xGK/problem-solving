bf05be0213445dd7fd08d9bbe136bfa7e246c279
Guho Kim <64512313+0xGK@users.noreply.github.com>
2024-12-31 18:16:20 +0900

    [D4] Title: 파핑파핑 지뢰찾기, Time: 310 ms, Memory: 54,944 KB -BaekjoonHub

--- SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
+++ SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.*;
 
 class Point{
@@ -14,46 +16,25 @@ class Solution
 	final static int MAX_N = 300;
 	static int N;
 	static char[][] graph = new char[MAX_N][MAX_N];
-	static char[][] solutionGraph = new char[MAX_N][MAX_N];
 	static int[] dx = {-1,0,1};
 	static int[] dy = {-1,0,1};
 	
-	public static void viewGraph(char[][] map) {
-		System.out.println();
+	public static void fillGraph() {
 		for(int i=0; i<N; i++) {
 			for(int j=0; j<N; j++) {
-				System.out.print(map[i][j]+" ");
-			}
-			System.out.println();
-		}
-	}
-	
-	public static void viewBoolean(Boolean[][] map) {
-		System.out.println();
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<N; j++) {
-				System.out.print(map[i][j]+" ");
-			}
-			System.out.println();
-		}
-	}
-	
-	public static void fillGraph(char[][] map) {
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<N; j++) {
-				if(map[i][j]=='.') {
-					map[i][j] = (char) (getNumber(map, i, j) + '0');
+				if(graph[i][j]=='.') {
+					graph[i][j] = (char) (getNumber(i, j) + '0');
 				}
 			}
 		}
 	}
-	public static int getNumber(char[][] map, int x, int y) {
+	public static int getNumber(int x, int y) {
 		int cnt = 0;
 		for(int i = x-1; i<= x+1; i++) {
 			for(int j = y-1; j <= y+1; j++) {
 				if(!inMap(i, j)) continue;
 				if(i==x && j==y) continue;
-				if(map[i][j]=='*') cnt++;
+				if(graph[i][j]=='*') cnt++;
 			}
 		}
 		return cnt;
@@ -64,11 +45,10 @@ class Solution
 		}
 		return false;
 	}
-	public static int bfs(char[][] graph, Boolean[][] isVisited, int startX, int startY) {
+	public static int bfs(Boolean[][] isVisited, int startX, int startY) {
 		Queue<Point> queue = new LinkedList<>();
 		queue.add(new Point(startX, startY));
 		isVisited[startX][startY]=true;
-//		cnt++;
 		while(!queue.isEmpty()) {
 			Point point = queue.poll();
 			int x = point.x;
@@ -76,15 +56,14 @@ class Solution
 			
 			for(int i=0; i<3; i++){
 				for(int j=0; j<3; j++) {
-					
 					int nx = x + dx[i];
 					int ny = y + dy[j];
 					if(inMap(nx, ny) && !isVisited[nx][ny]) {
-						if(solutionGraph[nx][ny]=='0') {
+						if(graph[nx][ny]=='0') {
 							isVisited[nx][ny] = true;
 							queue.add(new Point(nx, ny));						
 						}
-						else if('1'<=solutionGraph[nx][ny] && solutionGraph[nx][ny]<='8') {
+						else if('1'<=graph[nx][ny] && graph[nx][ny]<='8') {
 							isVisited[nx][ny] = true;
 						}
 					}
@@ -96,8 +75,8 @@ class Solution
 	public static void main(String args[]) throws Exception
 	{
         //System.setIn(new FileInputStream("res/input.txt"));
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
 		/*
 		 * bfs or dfs로 0인 부분을 true로 만들기 위해 몇 번 필요한지
 		 * 0과 인접한 부분의 숫자들도 모두 true
@@ -106,23 +85,20 @@ class Solution
 
 		for(int t=1; t<=T; ++t){
 			System.out.print("#" + t + " ");
-			N = sc.nextInt();
-			sc.nextLine();
+			N = Integer.parseInt(br.readLine());
 			
 			for(int i=0; i<N; i++) {
-				String input = sc.nextLine();
+				String input = br.readLine();
 				char[] inputChar = input.toCharArray();
 				for(int j=0; j<N; j++) {
 					graph[i][j] = inputChar[j];
-					solutionGraph[i][j] = inputChar[j];
 				}
 			}
-			fillGraph(solutionGraph);
-//			viewGraph(solutionGraph);
+			fillGraph();
 			Boolean[][] isVisited = new Boolean[N][N];
 			for(int i=0; i<N; i++) {
 				for(int j=0; j<N; j++) {
-					if(solutionGraph[i][j]=='*') {
+					if(graph[i][j]=='*') {
 						isVisited[i][j]=true;	// 지뢰는 조사하지 않을 것
 					}else {
 						isVisited[i][j]=false;					
@@ -133,14 +109,11 @@ class Solution
 			int ret = 0;
 			for(int i=0; i<N; i++) {
 				for(int j=0; j<N; j++) {
-					if(!isVisited[i][j] && solutionGraph[i][j]=='0') {
-						int tmp = bfs(graph, isVisited, i, j); 
-//						System.out.println("("+i+", "+j+"): "+tmp);
-						ret += tmp;
+					if(!isVisited[i][j] && graph[i][j]=='0') {  
+						ret += bfs(isVisited, i, j);
 					}
 				}
 			}
-//			viewBoolean(isVisited);
 			
 			for(int i=0; i<N; i++) {
 				for(int j=0; j<N; j++) {
@@ -152,6 +125,6 @@ class Solution
 			
 			System.out.println(ret);
 		}
-        sc.close();
+        br.close();
 	}
 }
