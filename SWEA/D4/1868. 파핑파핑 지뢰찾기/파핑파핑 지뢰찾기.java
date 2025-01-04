6c249c24294b739c9863b6108bfb32977ba4adce
Guho Kim <64512313+0xGK@users.noreply.github.com>
2025-01-04 13:00:33 +0900

    [D4] Title: 파핑파핑 지뢰찾기, Time: 223 ms, Memory: 36,684 KB -BaekjoonHub

--- SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
+++ SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
@@ -16,36 +16,30 @@ class Solution
 	final static int MAX_N = 300;
 	static int N;
 	static char[][] graph = new char[MAX_N][MAX_N];
-	static int[] dx = {-1,0,1};
-	static int[] dy = {-1,0,1};
+	static int[][] directions = {{-1,-1}, {-1,0}, {-1,1}, {0,-1}, {0,1}, {1,-1}, {1,0}, {1,1}};
 	
 	public static void fillGraph() {
 		for(int i=0; i<N; i++) {
 			for(int j=0; j<N; j++) {
 				if(graph[i][j]=='.') {
-					graph[i][j] = (char) (getNumber(i, j) + '0');
+					graph[i][j] = getNumber(i, j);
 				}
 			}
 		}
 	}
-	public static int getNumber(int x, int y) {
+	public static char getNumber(int x, int y) {
 		int cnt = 0;
-		for(int i = x-1; i<= x+1; i++) {
-			for(int j = y-1; j <= y+1; j++) {
-				if(!inMap(i, j)) continue;
-				if(i==x && j==y) continue;
-				if(graph[i][j]=='*') cnt++;
-			}
+		for(int[] dir : directions) {
+			int nx = x + dir[0];
+			int ny = y + dir[1];
+			if(inMap(nx, ny) && graph[nx][ny]=='*') cnt++;
 		}
-		return cnt;
+		return (char) (cnt + '0');
 	}
-	public static Boolean inMap(int x, int y) {
-		if(0 <= x && x < N && 0 <= y && y < N) {
-			return true;
-		}
-		return false;
+	public static boolean inMap(int x, int y) {
+		return 0 <= x && x < N && 0 <= y && y < N;
 	}
-	public static int bfs(Boolean[][] isVisited, int startX, int startY) {
+	public static int bfs(boolean[][] isVisited, int startX, int startY) {
 		Queue<Point> queue = new LinkedList<>();
 		queue.add(new Point(startX, startY));
 		isVisited[startX][startY]=true;
@@ -54,21 +48,21 @@ class Solution
 			int x = point.x;
 			int y = point.y;
 			
-			for(int i=0; i<3; i++){
-				for(int j=0; j<3; j++) {
-					int nx = x + dx[i];
-					int ny = y + dy[j];
-					if(inMap(nx, ny) && !isVisited[nx][ny]) {
-						if(graph[nx][ny]=='0') {
-							isVisited[nx][ny] = true;
-							queue.add(new Point(nx, ny));						
-						}
-						else if('1'<=graph[nx][ny] && graph[nx][ny]<='8') {
-							isVisited[nx][ny] = true;
-						}
+			for(int[] dir: directions) {
+				int nx = x + dir[0];
+				int ny = y + dir[1];
+				if(inMap(nx, ny) && !isVisited[nx][ny]) {
+					char v = graph[nx][ny];
+					if(v=='0') {
+						queue.add(new Point(nx, ny));						
+						isVisited[nx][ny] = true;
+					}
+					else if('1'<=v && v<='8') {
+						isVisited[nx][ny] = true;
 					}
 				}
 			}
+
 		}		
 		return 1;
 	}
@@ -77,14 +71,8 @@ class Solution
         //System.setIn(new FileInputStream("res/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-		/*
-		 * bfs or dfs로 0인 부분을 true로 만들기 위해 몇 번 필요한지
-		 * 0과 인접한 부분의 숫자들도 모두 true
-		 * 나머지 숫자들 true로 카운트
-		 */
 
 		for(int t=1; t<=T; ++t){
-			System.out.print("#" + t + " ");
 			N = Integer.parseInt(br.readLine());
 			
 			for(int i=0; i<N; i++) {
@@ -95,11 +83,11 @@ class Solution
 				}
 			}
 			fillGraph();
-			Boolean[][] isVisited = new Boolean[N][N];
+			boolean[][] isVisited = new boolean[N][N];
 			for(int i=0; i<N; i++) {
 				for(int j=0; j<N; j++) {
 					if(graph[i][j]=='*') {
-						isVisited[i][j]=true;	// 지뢰는 조사하지 않을 것
+						isVisited[i][j]=true;
 					}else {
 						isVisited[i][j]=false;					
 					}
@@ -123,7 +111,7 @@ class Solution
 				}
 			}
 			
-			System.out.println(ret);
+			System.out.println("#" + t + " " + ret);
 		}
         br.close();
 	}
