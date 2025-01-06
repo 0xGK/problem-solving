db729f528f8a9b609074f8fc7e61b960d88c4bb2
Guho Kim <64512313+0xGK@users.noreply.github.com>
2025-01-06 19:16:02 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 114 ms, Memory: 19,096 KB -BaekjoonHub

--- SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.*;
 
 class Solution
@@ -24,23 +26,25 @@ class Solution
 	public static void main(String args[]) throws Exception
 	{
         //System.setIn(new FileInputStream("res/input.txt"));
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
 
 
 		for(int t=1; t<=T; ++t){
 			System.out.print("#" + t + " ");
-			N = sc.nextInt();
-			B = sc.nextInt();
+			String[] line = br.readLine().split(" ");
+			N = Integer.parseInt(line[0]);
+			B = Integer.parseInt(line[1]);
 			ret = Integer.MAX_VALUE;
+			String[] line2 = br.readLine().split(" ");
 			for(int i=0; i<N; i++) {
-				H[i] = sc.nextInt();
+				H[i] = Integer.parseInt(line2[i]);
 			}
 			backtrack(0, 0);
 			System.out.println(ret-B);
 			
 
 		}
-        sc.close();
+        br.close();
 	}
 }
