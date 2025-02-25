02a38d53a113c0f9672d375154d1802643078ce2
Guho Kim <64512313+0xGK@users.noreply.github.com>
2025-02-25 20:12:39 +0900

    [Gold IV] Title: ⚾, Time: 412 ms, Memory: 12608 KB -BaekjoonHub

--- 백준/Gold/17281. ⚾/⚾.java
+++ 백준/Gold/17281. ⚾/⚾.java
@@ -1,55 +1,69 @@
 import java.util.*;
 import java.io.*;
 
+/*
+ * 1. 전체적인 순서는 1자리만 고정하고, 나머지는 팩토리얼
+ * 	-> backtrack 활용한 permutation 사용
+ * 	-> 순서 정해지면, 게임 플레이 
+ * 
+ * 2. 1~3루에 선수가 있거나 없거나
+ * 	-> 비트마스킹으로 접근 가능
+ * 		- 0000: no player
+ * 		- 0010: 1루 주자
+ * 		- ...
+ * 	-> 비트마스크 값을 left shift 하면서 밀어버릴 수 있음
+ * 	-> 4자리 보다 넘어가는 값을 주기적으로 계산해서 값 반영 가능
+ * 
+ * 
+ */
+
 public class Main {
 	static int N;
-	static int[] sequence;
+	static int[] memberOrder;
 	static int[][] memberResult;
 	static boolean[] isVisited;
 	static final int MEMBER_NUMBER = 9;
 	static int startIndex, maxScore;
 	
 	public static void init() {
-		sequence = new int[MEMBER_NUMBER];
-
-		// 4번타자 순서 fix
-		// initial startIndex+3 % MEMBER_NUMBER:= 0
-		startIndex=MEMBER_NUMBER-3;
-		sequence[0] = 1;
+		memberOrder = new int[MEMBER_NUMBER];
+		// 1번 선수 순서를 배열의 첫 자리에 fix
+		//	-> 참조하는 startIndex를 MEMBER_NUMBER-3부터 시작하면, 4번째에 1번 타자 
+		memberOrder[0] = 1;
 		isVisited = new boolean[MEMBER_NUMBER+1];
 		isVisited[1]=true;
-		
-		
 		memberResult = new int[N][MEMBER_NUMBER+1];
 		maxScore = Integer.MIN_VALUE;
 	}
 	
 	public static void playGame() {
+		// 4번 타자가, 1번 선수가 될 수 있도록 startIndex 조정
 		startIndex=MEMBER_NUMBER-3;
 		int score=0;
 		for(int round=0; round<N; round++) {
 			int bitmask = 0;
 			int outCount=0;
 			while(outCount<3) {
-				int playerNumber = sequence[startIndex%MEMBER_NUMBER];
+				int playerNumber = memberOrder[startIndex%MEMBER_NUMBER];
 				startIndex++;
 				int result = memberResult[round][playerNumber];
 				if(result == 0) {
 					outCount++;
 				}else if(result == 1){
+					// 홈에 선수 추가하고, 1칸씩 shift
 					bitmask = (bitmask+1) << 1;
-					
 				}else if(result == 2){
+					// 홈에 선수 추가하고, 2칸씩 shift					
 					bitmask = (bitmask+1) << 2;
-					
 				}else if(result == 3){
+					// 홈에 선수 추가하고, 3칸씩 shift					
 					bitmask = (bitmask+1) << 3;
-					
 				}else if(result == 4){
+					// 홈에 선수 추가하고, 4칸씩 shift
 					bitmask = (bitmask+1) << 4;
 				}
 				if(bitmask!=0) {
-					int temp = ((1<<4) - 1) << 4;
+					int temp = ~((1<<4) - 1);
 					score += Integer.bitCount(bitmask&temp);
 					bitmask = bitmask & ((1<<4)-1);
 				}
@@ -68,7 +82,7 @@ public class Main {
 		for(int index = 1; index<=MEMBER_NUMBER; index++) {
 			 if(isVisited[index]) continue;
 			 isVisited[index] = true;
-			 sequence[elementIndex] = index; 
+			 memberOrder[elementIndex] = index; 
 			 permutationBacktrack(elementIndex+1);
 			 isVisited[index] = false;
 		}
