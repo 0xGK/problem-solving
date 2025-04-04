9a33bb12e0a2f935696881d634a6edd8733f7204
Guho Kim <64512313+0xGK@users.noreply.github.com>
2025-04-04 20:13:15 +0900

    [Unrated] Title: [모의 SW 역량테스트] 점심 식사시간, Time: 110 ms, Memory: 27,136 KB -BaekjoonHub

--- SWEA/Unrated/2383. ［모의 SW 역량테스트］ 점심 식사시간/［모의 SW 역량테스트］ 점심 식사시간.java
+++ SWEA/Unrated/2383. ［모의 SW 역량테스트］ 점심 식사시간/［모의 SW 역량테스트］ 점심 식사시간.java
@@ -31,134 +31,135 @@ import java.util.*;
  */
 public class Solution  {
 	static BufferedReader br;
-	static StringTokenizer st;
-	
-	static int N, personSize;
-	static int[][] board;
-	static int totalMinTime;
-	static int[] stairTime;
-	
-	static int[][] timeToStairs;
-	static int[] selectedStairIdx;	// 0 또는 1이 들어가겠죠?
-	
-	static List<Integer>[] stairQueue;
-	static Queue<Integer> movingQueue;
-	
-	static int minTime;
-	
-	public static int getManhattanDistance(int[] first, int[] second) {
-		return Math.abs(first[0]-second[0])+Math.abs(first[1]-second[1]);
-	}
-	public static void init() throws IOException{
-		N = Integer.parseInt(br.readLine());
-		board = new int[N][N];
-		List<int[]> personLocationList = new ArrayList<>();
-		int[][] stairLocationList = new int[2][];
-		stairTime = new int[2];
-		int stairCnt=0;
-		for(int rowIdx=0; rowIdx<N; rowIdx++) {
-			st = new StringTokenizer(br.readLine());
-			for(int colIdx=0; colIdx<N; colIdx++) {
-				board[rowIdx][colIdx] = Integer.parseInt(st.nextToken());
-				if(board[rowIdx][colIdx]==1) {
-					personLocationList.add(new int[] {rowIdx, colIdx});
-				}
-				else if(board[rowIdx][colIdx]>1) {
-					stairTime[stairCnt] = board[rowIdx][colIdx];
-					stairLocationList[stairCnt++] = new int[] {rowIdx, colIdx};
-				}
-			}
-		}
-		personSize = personLocationList.size();
-		
-		selectedStairIdx = new int[personSize];
-		timeToStairs = new int[2][personSize];
-		
-		int personCnt=0;
-		for(int[] personLocation : personLocationList) {
-			for(int stairIdx=0; stairIdx<2; stairIdx++) {
-				int time = getManhattanDistance(stairLocationList[stairIdx], personLocation);
-				timeToStairs[stairIdx][personCnt] = time;
-			}
-			personCnt++;
-		}
-		stairQueue = new LinkedList[2];
-		minTime=Integer.MAX_VALUE;
-	}
-	
-	public static void playStairGame() {
-		stairQueue[0] = new LinkedList<>();
-		stairQueue[1] = new LinkedList<>();
-		
-		for(int personIdx=0; personIdx<personSize; personIdx++) {
-			int stairIdx = selectedStairIdx[personIdx];
-			stairQueue[stairIdx].add(timeToStairs[stairIdx][personIdx]);
-		}
-		
-		int maxTime = 0;
-		for(int stairIdx=0; stairIdx<2; stairIdx++) {
-			Collections.sort(stairQueue[stairIdx]);
-			movingQueue = new ArrayDeque<>();
-			int lastTime = 0;
-			
-			// 먼저 가능한 만큼 추가
-			// 대기 1분 추가
-			for(int personIdx=0; personIdx<3 && !stairQueue[stairIdx].isEmpty(); personIdx++) {
-				int escapeTime = stairQueue[stairIdx].remove(0) + stairTime[stairIdx] + 1;
-				movingQueue.add(escapeTime);
-			}
-			
-			while(!stairQueue[stairIdx].isEmpty()) {
-				int curTime = movingQueue.poll();
-				int nxtTime = stairQueue[stairIdx].remove(0);
-				
-				// 바로 들어가면 되는 경우
-				// nxt가 대기 중이었던 경우임.
-				if(curTime > nxtTime) {
-					movingQueue.add(curTime+stairTime[stairIdx]);
-				}
-				// 나중에 들어와야 하는 경우
-				// 대기 1분 추가
-				else {
-					movingQueue.add(nxtTime + stairTime[stairIdx]+1);
-				}
-			}
-			
-			while(!movingQueue.isEmpty()) {
-				lastTime = movingQueue.poll();
-			}
-			maxTime = maxTime < lastTime ? lastTime : maxTime;
-		}
-		minTime = minTime > maxTime ? maxTime : minTime;
-		
-		
-	}
-	
-	public static void combination(int selectedCnt) {
-		if(selectedCnt==personSize) {
-			playStairGame();
-			return;
-		}
-		
-		for(int stairIdx=0; stairIdx<2; stairIdx++) {
-			selectedStairIdx[selectedCnt] = stairIdx;
-			combination(selectedCnt+1);
-		}
-	}
-	
-	
+    static StringTokenizer st;
+     
+    static int N, personSize;
+    static int[][] board;
+    static int totalMinTime;
+    static int[] stairTime;
+     
+    static int[][] timeToStairs;
+    static int[] selectedStairIdx;  // 0 또는 1이 들어가겠죠?
+     
+    static List<Integer>[] stairQueue;
+    static Queue<Integer> movingQueue;
+     
+    static int minTime;
+     
+    public static int getManhattanDistance(int[] first, int[] second) {
+        return Math.abs(first[0]-second[0])+Math.abs(first[1]-second[1]);
+    }
+    public static void init() throws IOException{
+        N = Integer.parseInt(br.readLine());
+        board = new int[N][N];
+        List<int[]> personLocationList = new ArrayList<>();
+        int[][] stairLocationList = new int[2][];
+        stairTime = new int[2];
+        int stairCnt=0;
+        for(int rowIdx=0; rowIdx<N; rowIdx++) {
+            st = new StringTokenizer(br.readLine());
+            for(int colIdx=0; colIdx<N; colIdx++) {
+                board[rowIdx][colIdx] = Integer.parseInt(st.nextToken());
+                if(board[rowIdx][colIdx]==1) {
+                    personLocationList.add(new int[] {rowIdx, colIdx});
+                }
+                else if(board[rowIdx][colIdx]>1) {
+                    stairTime[stairCnt] = board[rowIdx][colIdx];
+                    stairLocationList[stairCnt++] = new int[] {rowIdx, colIdx};
+                }
+            }
+        }
+        personSize = personLocationList.size();
+         
+        selectedStairIdx = new int[personSize];
+        timeToStairs = new int[2][personSize];
+         
+        int personCnt=0;
+        for(int[] personLocation : personLocationList) {
+            for(int stairIdx=0; stairIdx<2; stairIdx++) {
+                timeToStairs[stairIdx][personCnt] = getManhattanDistance(stairLocationList[stairIdx], personLocation);
+            }
+            personCnt++;
+        }
+        stairQueue = new ArrayList[2];
+        minTime=Integer.MAX_VALUE;
+    }
+     
+    public static void playStairGame() {
+        stairQueue[0] = new ArrayList<>();
+        stairQueue[1] = new ArrayList<>();
+         
+        for(int personIdx=0; personIdx<personSize; personIdx++) {
+            int stairIdx = selectedStairIdx[personIdx];
+            stairQueue[stairIdx].add(timeToStairs[stairIdx][personIdx]);
+        }
+        Collections.sort(stairQueue[0]);
+        Collections.sort(stairQueue[1]);
+         
+        int maxTime = 0;
+        for(int stairIdx=0; stairIdx<2; stairIdx++) {
+            movingQueue = new ArrayDeque<>();
+            int lastTime = 0;
+             
+            // 먼저 가능한 만큼 추가
+            // 대기 1분 추가
+            int personIdx = 0;
+            for(;personIdx<3 && personIdx < stairQueue[stairIdx].size(); personIdx++) {
+                movingQueue.add(stairQueue[stairIdx].get(personIdx) + stairTime[stairIdx] + 1);
+            }
+             
+            while(personIdx < stairQueue[stairIdx].size()) {
+                int curTime = movingQueue.poll();
+                int nxtTime = stairQueue[stairIdx].get(personIdx++);
+                 
+                // 바로 들어가면 되는 경우
+                // nxt가 대기 중이었던 경우임.
+                if(curTime > nxtTime) {
+                    movingQueue.add(curTime+stairTime[stairIdx]);
+                }
+                // 나중에 들어와야 하는 경우
+                // 대기 1분 추가
+                else {
+                    movingQueue.add(nxtTime + stairTime[stairIdx]+1);
+                }
+            }
+             
+            while(!movingQueue.isEmpty()) {
+                lastTime = movingQueue.poll();
+            }
+            maxTime = maxTime < lastTime ? lastTime : maxTime;
+        }
+        minTime = minTime > maxTime ? maxTime : minTime;
+         
+         
+    }
+     
+    public static void combination(int selectedCnt) {
+        if(selectedCnt==personSize) {
+            playStairGame();
+            return;
+        }
+         
+        for(int stairIdx=0; stairIdx<2; stairIdx++) {
+            selectedStairIdx[selectedCnt] = stairIdx;
+            combination(selectedCnt+1);
+        }
+    }
+     
+     
     public static void main(String[] args) throws Exception {
+        //System.setIn(new FileInputStream("input.txt"));
         br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-		
+         
         int T = Integer.parseInt(br.readLine());
         for(int testCase=1; testCase<=T; ++testCase) {
-        	init();
-        	combination(0);
+            init();
+            combination(0);
             sb.append('#').append(testCase).append(' ').append(minTime).append('\n');
-            
+             
         }
         System.out.println(sb);
-         
+          
     }
 }
